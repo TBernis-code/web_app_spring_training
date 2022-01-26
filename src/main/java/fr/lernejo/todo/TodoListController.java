77f5c8efc4980637e4b8b1a3ddd3cfa21a23dd07
Tony Bernis <tony.bernis@kdetude.com>
2022-01-26 09:56:27 +0100

    Partie 7 - Persistence en base de données

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -1,27 +1,27 @@
 package fr.lernejo.todo;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
-public class TodoListController {
-    private final static List<Todo> todos = new ArrayList<Todo>();
-
-    @GetMapping("/api/todo")
-    public ResponseEntity<List<Todo>> getTodos() {
-        return new ResponseEntity<List<Todo>>(todos, HttpStatus.OK);
+public class TodoListController  {
+    TodoRepository repo;
+    public TodoListController(TodoRepository rep)
+    {
+        this.repo=rep;
     }
+    @PostMapping(value = "/api/todo")
+    public TodoEntity ajout(@RequestBody TodoEntity todo)
+    {
+        return repo.save(todo);
 
-    @PostMapping(value = "/api/todo", consumes = {"application/json"})
-    public ResponseEntity<Todo> addTodo(@RequestBody Todo todo) {
-        todos.add(todo);
-        return new ResponseEntity<Todo>(todo, HttpStatus.CREATED);
+    }
+    @GetMapping(value = "/api/todo")
+    public Iterable<TodoEntity> affichage()
+    {
+        return repo.findAll();
+
     }
 }
